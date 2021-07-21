cbc0ed0cf0a2850805cca33ef38ac8402d81ead5
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-07-21 11:20:53 +0530

    Players moving detection completed

--- sockets/classes/PlayerConfig.js
+++ sockets/classes/PlayerConfig.js
@@ -2,7 +2,7 @@ class PlayerConfig {
   constructor(settings) {
     this.xVector = 0;
     this.yVector = 0;
-    this.speed = settings.defauleSpeed;
+    this.speed = settings.defaultSpeed;
     this.zoom = settings.defaultZoom;
   }
 }
--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -7,6 +7,7 @@ const PlayerData = require("./classes/PlayerData");
 const PlayerConfig = require("./classes/PlayerConfig");
 
 let orbs = [];
+let players = [];
 let settings = {
   defaultOrbs: 500,
   defaultSpeed: 6,
@@ -19,18 +20,53 @@ let settings = {
 initGame();
 
 io.sockets.on("connect", (socket) => {
+  let player = {};
   // works when a player is connected
-
   socket.on("init", (data) => {
+    socket.join("game");
+
     let playerConfig = new PlayerConfig(settings);
 
     let playerData = new PlayerData(data.playerName, settings);
 
-    let player = new Player(socket.id, playerConfig, playerData);
+    player = new Player(socket.id, playerConfig, playerData);
+
+    setInterval(() => {
+      io.to("game").emit("tock", {
+        players,
+        playerX: player.playerData.locX,
+        playerY: player.playerData.locY,
+      });
+    }, 33); // there are 30 33's in 1000 milliseconds or 1/30th of a second or 1 of 30 FPS
 
     socket.emit("initReturn", {
       orbs,
     });
+
+    players.push(playerData);
+  });
+
+  socket.on("tick", (data) => {
+    speed = player.playerConfig.speed;
+    // update playerConfig object with new direction in data
+    // also create local variable for readability
+    xV = player.playerConfig.xVector = data.xVector;
+    yV = player.playerConfig.xVector = data.yVector;
+
+    if (
+      (player.playerData.locX < 5 && player.playerData.xVector < 0) ||
+      (player.playerData.locX > 500 && xV > 0)
+    ) {
+      player.playerData.locY -= speed * yV;
+    } else if (
+      (player.playerData.locY < 5 && yV > 0) ||
+      (player.playerData.locY > 500 && yV < 0)
+    ) {
+      player.playerData.locX += speed * xV;
+    } else {
+      player.playerData.locX += speed * xV;
+      player.playerData.locY -= speed * yV;
+    }
   });
 });
 
